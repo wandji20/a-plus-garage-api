042ce13ed30bec5ecfff1794bc3edcb1b98bcdce
wandji20 <wbertrand10@gmail.com>
2021-07-15 22:39:11 +0100

    Add request spec for admins

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,5 +2,5 @@ class Admin < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { in: 3..10 }
   validates :email, presence: true, uniqueness: true
-  validates :adminID, presence: true, uniqueness: true, length: { in: 8..18 }
+  validates :adminID, presence: true, uniqueness: true, length: { in: 6..16 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
       end
       resources :parts
       resources :categories
+      resources :admins
     end
   end
-  resources :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
