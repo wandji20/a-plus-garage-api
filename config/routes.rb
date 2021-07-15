c3425ca156e84851c3e4166dffab247b2435786d
wandji20 <wbertrand10@gmail.com>
2021-07-15 15:29:08 +0100

    Add category scaffold and migrate db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   resources :cars
   namespace :api do
     namespace :v1 do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_15_142516) do
+ActiveRecord::Schema.define(version: 2021_07_15_142803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 2021_07_15_142516) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "fuel_rate"
     t.integer "horse_power"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
