470236b87382c87ed4401d7fa808b86f8f624047
wandji20 <wbertrand10@gmail.com>
2021-07-28 20:33:52 +0100

    Add cors gem and update config

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,13 @@ module APlusGarageApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
