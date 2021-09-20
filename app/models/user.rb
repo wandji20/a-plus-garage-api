4f455d4960be681e324ad9e818fa54cb7043c9f3
wandji20 <wbertrand10@gmail.com>
2021-09-20 22:25:23 +0100

    Fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :cars, dependent: :destroy
   validates :name, presence: true, length: { in: 3..10 }
   validates :user_name, presence: true, uniqueness: true, length: { in: 3..10 }
-  validates :password, length: {in: 3..10 }
+  validates :password, length: { in: 3..10 }
 
   has_secure_password
 
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,19 +5,20 @@ RSpec.describe 'Users API', type: :request do
   let(:headers) { valid_headers.except('Authorization') }
 
   let(:valid_attributes) do
-    {:user => { name: user.name, user_name: user.user_name, password: user.password,password_confirmation: user.password }}
+    { user: { name: user.name, user_name: user.user_name, password: user.password,
+              password_confirmation: user.password } }
   end
-  let(:invalid_attributes) do 
+  let(:invalid_attributes) do
     attributes_for(:user)
   end
 
   describe 'POST /create' do
     context 'when valid request' do
-      before { 
+      before do
         post 'http://localhost:3001/signup',
-        params: valid_attributes.to_json, 
-        headers: headers 
-      }
+             params: valid_attributes.to_json,
+             headers: headers
+      end
 
       it 'creates a new user' do
         expect(response).to have_http_status(201)
@@ -33,7 +34,7 @@ RSpec.describe 'Users API', type: :request do
     end
 
     context 'when invalid request' do
-      before { post 'http://localhost:3001/signup', params: { :user => { name: user.name } }.to_json, headers: headers }
+      before { post 'http://localhost:3001/signup', params: { user: { name: user.name } }.to_json, headers: headers }
 
       it 'does not create a new user' do
         expect(response).to have_http_status(422)
